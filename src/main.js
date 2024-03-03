0812f33ae853910e15c49a4f9bf4a90033b0824c
CCC200 <megamanzero94@hotmail.com>
2024-03-03 10:16:23 -0500

    Add update check

--- src/main.js
+++ src/main.js
@@ -20,7 +20,26 @@ function injectCSSUrl(URL) {
         window.alert("ERROR: " + e.message);
     }
 }
+function checkForUpdate(remote) {
+    const package = require('./package.json');
+    if(package.version != remote) {
+        const update = window.confirm("𝗡𝗲𝘄 𝗰𝗹𝗶𝗲𝗻𝘁 𝘂𝗽𝗱𝗮𝘁𝗲: " + remote + "\nWould you like to download the updated client?");
+        if(update) nw.Shell.openExternal("https://github.com/letsgodigi/letsgodigi-desktop-client/releases/latest");
+    }
+}
+function getRemoteVersion() {
+    var oReq = new XMLHttpRequest();
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", function() {
+        const js = JSON.parse(this.responseText);
+        checkForUpdate(js.version);
+    });
+    oReq.open("GET", "https://raw.githubusercontent.com/letsgodigi/letsgodigi-desktop-client/main/src/package.json");
+    oReq.timeout = 5000;
+    oReq.send();
+}
 //Injection start
+getRemoteVersion();
 const dwin = nw.Window.get();
 dwin.on('loaded', function(dwindow) {
     injectCSSUrl("https://letsgodigi.github.io/assets/custom.css");
